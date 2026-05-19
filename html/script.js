12f853d8165b88a139b45641b5f3e6f31ffa35e3
himanshim-dev00 <himanshimishra99362gmail.com>
2026-05-19 22:58:01 +0530

    js deleted

--- html/script.js
+++
@@ -1,7 +0,0 @@
-control+shift+j= open console
-console.log(hello js ) that means console me print ho jayega hello js 
-console.warn means warning 
-console.error means error 
-console.table means table form 
-
-
