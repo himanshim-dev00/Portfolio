b7478fc194f0bb88fee0fbcbbc49946639f0a656
himanshim-dev00 <himanshimishra99362gmail.com>
2026-05-19 22:51:00 +0530

    UI design a5

---
+++ html/script.js
@@ -0,0 +1,7 @@
+control+shift+j= open console
+console.log(hello js ) that means console me print ho jayega hello js 
+console.warn means warning 
+console.error means error 
+console.table means table form 
+
+
