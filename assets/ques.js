ca67b95c9ee8c45e1f97f85ed1ef8d784b6277f9
himanshim-dev00 <himanshimishra99362gmail.com>
2026-05-28 21:17:37 +0530

    Tea leaves website

---
+++ assets/ques.js
@@ -0,0 +1,46 @@
+1.// Print'Hello Javascript' in Console
+console.log('Hello Javascript');
+
+2.Print your name, age and city using one console.log()
+console.log('Name: Himanshi Mishra, Age: 26, City: Kanpur');
+
+3. print a warning message in console.warn
+console.warn('This is a warning message.'); 
+
+4. print an error mesage in console.error()
+console.error('This is an error message.');
+
+5. use console.table() to display an array of 5 numbers 
+console.table( 10,20,30,40,50 );
+
+6. Create a variable called `studentName` and store your name in it.
+      let studentName = 'Himanshi Mishra';
+7. Create a variable age and print it.
+     let age = 26;
+     console.log(age);
+ 8. Create two variables and swap their values.    
+        let a=5;
+        b=6;
+        let temp=a;
+          a=b;
+          b=temp;
+          console.log(a,b);
+  9.4. Create a constant variable for PI and print it.
+const PI = 3.14159;
+console.log(PI);
+10. Declare a variable without assigning a value and print it.
+let emptyValue;
+console.log(emptyValue);
+
+11. Create a variable score and increase it by 10.
+let score = 50;
+score += 10;
+
+console.log(score);
+
+12. Create three variables for first name, last name, and full name.
+let firstName = "Himanshi";
+let lastName = "Mishra";
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);          
